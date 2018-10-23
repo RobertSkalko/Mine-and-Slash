0853af05560fb6858127bc804c501338f019735d
robert trebor <treborx555@gmail.com>
2018-10-23 17:09:16 +0200

    fixed slime issue by changing to EntityJoinWorldEvent. this should make all mobs work.

--- src/main/java/com/robertx22/onevent/Combat/OnMobSpawn.java
+++ src/main/java/com/robertx22/onevent/Combat/OnMobSpawn.java
@@ -5,10 +5,10 @@ import com.robertx22.uncommon.capability.EntityData;
 import com.robertx22.uncommon.datasaving.UnitSaving;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.living.LivingHealEvent;
-import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -21,66 +21,34 @@ public class OnMobSpawn {
 	}
 
 	@SubscribeEvent
-	public static void onMobSpawn(LivingSpawnEvent.CheckSpawn event) {
+	public static void onMobSpawn(EntityJoinWorldEvent event) {
+
+		if (!(event.getEntity() instanceof EntityLivingBase)) {
+			return;
+		}
+
+		EntityLivingBase entity = (EntityLivingBase) event.getEntity();
 
 		if (Minecraft.getMinecraft().player == null) {
 			return;
 		}
-		if (event.getEntityLiving().world.isRemote) {
+		if (entity.world.isRemote) {
 			return;
 		}
-		if (!event.getEntityLiving().hasCapability(EntityData.Data, null)) {
+		if (!entity.hasCapability(EntityData.Data, null)) {
 			return;
 		}
 
-		if (!(event.getEntityLiving() instanceof EntityPlayer)) {
-			EntityLiving mob = (EntityLiving) event.getEntityLiving();
-			Unit unit = Unit.Mob(mob);
-			UnitSaving.Save(mob, unit);
+		Unit check = UnitSaving.Load(entity);
+
+		if (!(entity instanceof EntityPlayer) && check == null) {
+			Unit unit = Unit.Mob(entity);
+			UnitSaving.Save(entity, unit);
 
 		}
 
-		event.getEntityLiving().setHealth(5000);
+		entity.setHealth(200);
 
-		// System.out.println("Saved unit to mob");
-
-		/*
-		 * 
-		 * if (event.getEntityLiving().hasCapability(EntityData.Data, null) &&
-		 * !event.getEntityLiving() .getCapability(EntityData.Data,
-		 * null).getNBT().getBoolean(Tags.ENTITY_INFO)) {
-		 * 
-		 * EntityLiving mob = (EntityLiving) event.getEntityLiving();
-		 * 
-		 * EntityData.IData data = mob.getCapability(EntityData.Data, null);
-		 * 
-		 * NBTTagCompound nbt = GeneralUtils.getdefaultEntityNBT();
-		 * 
-		 * int rarity = RandomUtils.rollArray(Chances.MOB_SPAWN_RARITY);
-		 * 
-		 * int lvl = Mob.getRandomLevel(event.getWorld().playerEntities.get(0));
-		 * 
-		 * int hp = (int) (mob.getMaxHealth() * lvl * Mob.rarityHPMulti[rarity] *
-		 * ModConfig.Cheats.DIFFICULTY); int dmg = (int) (10 * lvl *
-		 * Mob.rarityDMGMulti[rarity] / 4 * ModConfig.Cheats.DIFFICULTY);
-		 * 
-		 * nbt.setInteger(com.robertx22.Stats.HEALTH.name, hp);
-		 * nbt.setInteger(Tags.LEVEL, lvl); nbt.setInteger(Tags.RARITY_NUMBER, rarity);
-		 * nbt.setString(Tags.RARITY, Mob.rarityNames[rarity]);
-		 * nbt.setBoolean(Tags.ENTITY_INFO, true); nbt.setInteger(Tags.DAMAGE, dmg);
-		 * 
-		 * mob.setCustomNameTag(Gear.rarityChatColors[rarity] + "" + " " + "LvL " + lvl
-		 * + " " + Mob .rarityNames[rarity] + " " + mob.getName());
-		 * 
-		 * mob.setAlwaysRenderNameTag(true);
-		 * 
-		 * data.setNBT(nbt);
-		 * 
-		 * event.setResult(Event.Result.ALLOW);
-		 * 
-		 * }
-		 * 
-		 */
 	}
 
 }
