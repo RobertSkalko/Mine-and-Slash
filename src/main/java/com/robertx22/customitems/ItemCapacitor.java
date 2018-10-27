87da77b8482538264dc8acaf65fbafe85647e081
robert trebor <treborx555@gmail.com>
2018-10-27 16:41:07 +0200

    added json recipes, they seem neat when outcomes aren't random. less code pollution!

--- src/main/java/com/robertx22/customitems/ItemCapacitor.java
+++ src/main/java/com/robertx22/customitems/ItemCapacitor.java
@@ -4,16 +4,24 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.annotation.Nullable;
+
 import com.robertx22.customitems.currency.CurrencyItem;
 import com.robertx22.database.lists.Rarities;
+import com.robertx22.database.rarities.ItemRarity;
 import com.robertx22.mmorpg.Ref;
 import com.robertx22.uncommon.utilityclasses.RegisterUtils;
 
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @EventBusSubscriber
 public class ItemCapacitor extends Item {
@@ -37,6 +45,20 @@ public class ItemCapacitor extends Item {
 
 		return RepairValues.get(rarity);
 
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+		try {
+			ItemRarity rar = Rarities.Items.get(this.rarity);
+			stack.setStackDisplayName(rar.Color() + rar.Name() + " Capacitor");
+
+			tooltip.add("Put inside a Repair Stationn to");
+			tooltip.add("decrease fuel consumption by " + this.GetFuelMultiplier() + "x");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@SubscribeEvent
--- src/main/java/com/robertx22/customitems/ores/ItemOre.java
+++ src/main/java/com/robertx22/customitems/ores/ItemOre.java
@@ -4,22 +4,30 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.annotation.Nullable;
+
 import com.robertx22.customitems.blocks.BlockOre;
 import com.robertx22.customitems.currency.CurrencyItem;
 import com.robertx22.database.lists.Rarities;
+import com.robertx22.database.rarities.ItemRarity;
 import com.robertx22.mmorpg.Ref;
 import com.robertx22.uncommon.utilityclasses.RegisterUtils;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @EventBusSubscriber
 public class ItemOre extends Item {
@@ -44,6 +52,21 @@ public class ItemOre extends Item {
 		maxStackSize = 64;
 		setCreativeTab(CurrencyItem.CurrencyTab);
 		this.rarity = rarity;
+
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+		try {
+			ItemRarity rar = Rarities.Items.get(this.rarity);
+			stack.setStackDisplayName(rar.Color() + rar.Name() + " Ore");
+
+			tooltip.add("Precious Ore used to repair gear.");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
