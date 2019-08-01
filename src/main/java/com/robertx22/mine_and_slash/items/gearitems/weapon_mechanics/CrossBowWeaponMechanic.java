a05b611029c388112849c02b5765a19f79784568
robert trebor <treborx555@gmail.com>
2019-08-01 13:00:58 +0200

    2.3.2 fix crossbow dmg

--- src/main/java/com/robertx22/mine_and_slash/items/gearitems/weapon_mechanics/CrossBowWeaponMechanic.java
+++ src/main/java/com/robertx22/mine_and_slash/items/gearitems/weapon_mechanics/CrossBowWeaponMechanic.java
@@ -1,8 +1,10 @@
 package com.robertx22.mine_and_slash.items.gearitems.weapon_mechanics;
 
 import com.robertx22.mine_and_slash.items.gearitems.bases.WeaponMechanic;
+import com.robertx22.mine_and_slash.uncommon.capability.EntityCap;
 import com.robertx22.mine_and_slash.uncommon.effectdatas.interfaces.WeaponTypes;
 import com.robertx22.mine_and_slash.uncommon.localization.Styles;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 
@@ -21,6 +23,15 @@ public class CrossBowWeaponMechanic extends WeaponMechanic {
     @Override
     public WeaponTypes weaponType() {
         return WeaponTypes.CrossBow;
+    }
+
+    @Override
+    public boolean Attack(LivingEntity source, LivingEntity target,
+                          EntityCap.UnitData unitsource, EntityCap.UnitData targetUnit) {
+
+        super.multiplyDamage(source, target, unitsource, targetUnit, 2);
+
+        return true;
     }
 
 }
