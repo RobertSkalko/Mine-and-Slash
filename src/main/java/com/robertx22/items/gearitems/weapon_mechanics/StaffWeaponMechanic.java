0d1d6b566ac83bd8a29d73fd9253d33b23765111
AzureZhen <rcraft@cyberconnects2.com>
2019-09-28 22:10:17 -0400

    code clean up

--- src/main/java/com/robertx22/items/gearitems/weapon_mechanics/StaffWeaponMechanic.java
+++ src/main/java/com/robertx22/items/gearitems/weapon_mechanics/StaffWeaponMechanic.java
@@ -9,23 +9,23 @@ import net.minecraft.entity.EntityLivingBase;
 
 public class StaffWeaponMechanic extends WeaponMechanic {
 
-    @Override
-    public float GetEnergyCost() {
-	return ModConfig.BasePlayerStats.staff_energy_cost;
-    }
+	@Override
+	public float GetEnergyCost() {
+		return ModConfig.BasePlayerStats.staff_energy_cost;
+	}
 
-    @Override
-    public WeaponTypes weaponType() {
-	return WeaponTypes.Staff;
-    }
+	@Override
+	public WeaponTypes weaponType() {
+		return WeaponTypes.Staff;
+	}
 
-    @Override
-    public boolean Attack(EntityLivingBase source, EntityLivingBase target, UnitData unitsource, UnitData targetUnit) {
+	@Override
+	public boolean Attack(EntityLivingBase source, EntityLivingBase target, UnitData unitsource, UnitData targetUnit) {
 
-	// double attack :3
-	super.Attack(source, target, unitsource, targetUnit);
-	super.Attack(source, target, unitsource, targetUnit);
+		// double attack :3
+		super.Attack(source, target, unitsource, targetUnit);
+		super.Attack(source, target, unitsource, targetUnit);
 
-	return true;
-    }
+		return true;
+	}
 }
