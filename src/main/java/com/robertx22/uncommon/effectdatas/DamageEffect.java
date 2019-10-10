46e499e520e5fa63a1b9a0134ce3b05c9c937b1b
AzureZhen <rcraft@cyberconnects2.com>
2019-10-10 17:56:40 -0400

    Updated #7 PR to the new code I did.

--- src/main/java/com/robertx22/uncommon/effectdatas/DamageEffect.java
+++ src/main/java/com/robertx22/uncommon/effectdatas/DamageEffect.java
@@ -3,6 +3,7 @@ package com.robertx22.uncommon.effectdatas;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
+import com.robertx22.api.MineAndSlashEvents;
 import com.robertx22.config.ModConfig;
 import com.robertx22.database.stat_types.defense.BlockStrength;
 import com.robertx22.mmorpg.Main;
@@ -31,6 +32,7 @@ import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 
 public class DamageEffect extends EffectData
@@ -144,6 +146,8 @@ public class DamageEffect extends EffectData
 
 			}
 		}
+		
+		MinecraftForge.EVENT_BUS.post(new MineAndSlashEvents.DamageEvent(Source, Target, Number, dmgsource, isBlocked()));
 
 	}
 
