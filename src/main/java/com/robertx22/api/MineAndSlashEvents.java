15277bf36fe3c02acb81583332c7ecf732a33c22
AzureDoom <jerry99666@gmail.com>
2019-10-10 17:48:35 -0400

    Revert "Add damage event"

--- src/main/java/com/robertx22/api/MineAndSlashEvents.java
+++ src/main/java/com/robertx22/api/MineAndSlashEvents.java
@@ -1,12 +1,8 @@
 package com.robertx22.api;
 
-import com.robertx22.spells.bases.MyDamageSource;
 import com.robertx22.uncommon.capability.EntityData.UnitData;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.event.entity.living.LivingEvent;
-import net.minecraftforge.fml.common.eventhandler.Event;
 
 public class MineAndSlashEvents {
 
@@ -23,78 +19,5 @@ public class MineAndSlashEvents {
 
     }
 
-  }
-
-  /**
-   * Fired when a {@link com.robertx22.uncommon.effectdatas.DamageEffect} is activated.
-   * <p>
-   * Editing the damage source will not affect the damage applied.
-   */
-  public static class DamageEvent extends Event {
-
-    private final EntityLivingBase source;
-    private final EntityLivingBase target;
-    private final float totalDamage;
-    private final MyDamageSource damageSource;
-    private final boolean fullyBlocked;
-
-    public DamageEvent(EntityLivingBase source, EntityLivingBase target, float totalDamage, MyDamageSource damageEffect, boolean fullyBlocked) {
-
-      this.source = source;
-      this.target = target;
-      this.totalDamage = totalDamage;
-      this.damageSource = damageEffect;
-      this.fullyBlocked = fullyBlocked;
-    }
-
-    public EntityLivingBase getSource() {
-
-      return this.source;
-    }
-
-    public EntityLivingBase getTarget() {
-
-      return this.target;
-    }
-
-    /**
-     * @return the total damage dealt before blocking
-     */
-    public float getTotalDamage() {
-
-      return this.totalDamage;
-    }
-
-    /**
-     * @return the actual damage dealt after blocking
-     */
-    public float getAdjustedDamage() {
-
-      return this.damageSource.realDamage;
-    }
-
-    /**
-     * @return the amount of damage blocked
-     */
-    public float getBlockedDamage() {
-
-      return this.fullyBlocked ? this.totalDamage : this.totalDamage - this.damageSource.realDamage;
-    }
-
-    /**
-     * @return the damage source
-     */
-    public MyDamageSource getDamageSource() {
-
-      return this.damageSource;
-    }
-
-    /**
-     * @return true if all damage was blocked
-     */
-    public boolean isFullyBlocked() {
-
-      return this.fullyBlocked;
-    }
   }
 }
--- src/main/java/com/robertx22/uncommon/effectdatas/DamageEffect.java
+++ src/main/java/com/robertx22/uncommon/effectdatas/DamageEffect.java
@@ -3,7 +3,6 @@ package com.robertx22.uncommon.effectdatas;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
-import com.robertx22.api.MineAndSlashEvents;
 import com.robertx22.config.ModConfig;
 import com.robertx22.database.stat_types.defense.BlockStrength;
 import com.robertx22.mmorpg.Main;
@@ -30,7 +29,6 @@ import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
-import net.minecraftforge.common.MinecraftForge;
 
 public class DamageEffect extends EffectData
 		implements IArmorReducable, IPenetrable, IDamageEffect, IElementalResistable, IElementalPenetrable, ICrittable {
@@ -123,7 +121,6 @@ public class DamageEffect extends EffectData
 			}
 		}
 
-		MinecraftForge.EVENT_BUS.post(new MineAndSlashEvents.DamageEvent(Source, Target, Number, dmgsource, fullyblocked));
 	}
 
 	private void RestoreMana() {
