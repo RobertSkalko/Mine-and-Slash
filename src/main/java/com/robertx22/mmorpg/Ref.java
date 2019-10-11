a81f93a65f4e7ef9c7e2efe31b12456fef6cd3c0
AzureZhen <rcraft@cyberconnects2.com>
2019-10-10 20:25:45 -0400

    Revert "v1.8.0"
    This reverts commit 3f7cb3d51918415fa5148f14ca1660d04d15a7e1.

--- src/main/java/com/robertx22/mmorpg/Ref.java
+++ src/main/java/com/robertx22/mmorpg/Ref.java
@@ -3,7 +3,7 @@ package com.robertx22.mmorpg;
 public class Ref {
 
 	public static final String MODID = "mmorpg";
-	public static final String VERSION = "1.8.0";
+	public static final String VERSION = "1.7.8";
 	public static final String NAME = "Mine and Slash Reloaded";
 	public static final String DESC = "Turn Minecraft into a hack and slash looter!";
 
