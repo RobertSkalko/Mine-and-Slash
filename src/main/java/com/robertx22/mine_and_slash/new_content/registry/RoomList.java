dbfdeabd0c4bdd18d4fbcceabd4056f688f60e77
robert trebor <treborx555@gmail.com>
2020-02-29 10:44:12 +0100

    more rooms

--- src/main/java/com/robertx22/mine_and_slash/new_content/registry/RoomList.java
+++ src/main/java/com/robertx22/mine_and_slash/new_content/registry/RoomList.java
@@ -27,6 +27,13 @@ public class RoomList {
             rooms.add(new DungeonRoom("simple0", RoomType.STRAIGHT_HALLWAY, RoomGroup.STONE_BRICK));
             rooms.add(new DungeonRoom("simple_maze0", RoomType.TRIPLE_HALLWAY, RoomGroup.STONE_BRICK));
 
+            rooms.add(new DungeonRoom("trap_puzzle0", RoomType.CURVED_HALLWAY, RoomGroup.SANDSTONE));
+            rooms.add(new DungeonRoom("trap_prison0", RoomType.END, RoomGroup.SANDSTONE));
+            //rooms.add(new DungeonRoom("basic", RoomType.ENTRANCE, RoomGroup.STONE_BRICK));
+            rooms.add(new DungeonRoom("simple0", RoomType.FOUR_WAY, RoomGroup.SANDSTONE));
+            rooms.add(new DungeonRoom("simple0", RoomType.STRAIGHT_HALLWAY, RoomGroup.SANDSTONE));
+            rooms.add(new DungeonRoom("simple_maze0", RoomType.TRIPLE_HALLWAY, RoomGroup.SANDSTONE));
+
             rooms.add(new DungeonRoom("simple_prismarine", RoomType.FOUR_WAY, RoomGroup.MISC));
             rooms.add(new DungeonRoom("prismarine", RoomType.ENTRANCE, RoomGroup.MISC));
             rooms.add(new DungeonRoom("nether0", RoomType.END, RoomGroup.MISC));
